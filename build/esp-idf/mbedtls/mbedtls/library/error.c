0d01b58d1552813310c132057762a5f5809c5da0
rickhalz <91548246+rickhalz@users.noreply.github.com>
2025-03-22 11:11:54 +0700

    Delete build directory

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/albert/test/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/home/albert/test/esp/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
